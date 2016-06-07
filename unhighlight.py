2e84ab989d747db10b39e57af705bd24b8a0ef28
anrodger <me@andrew.rs>
2016-06-07 19:14:53 -0400

    License change to GPL3
    Because Weechat actually complains if the plugin license and script
    license are different. Seriously.

--- unhighlight.py
+++ unhighlight.py
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2016 Andrew Rodgers-Schatz <me@andrew.rs>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 try:
     import weechat
 except Exception:
@@ -11,7 +27,7 @@ import re
 SCRIPT_NAME     = 'unhighlight'
 SCRIPT_AUTHOR   = 'xiagu'
 SCRIPT_VERSION  = '0.1.0'
-SCRIPT_LICENSE  = 'MIT'
+SCRIPT_LICENSE  = 'GPL3'
 SCRIPT_DESC     = 'Allows per-buffer specification of a regex that prevents highlights.'
 
 
